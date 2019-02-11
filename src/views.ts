f116d1560b84a59dbfa6ee1ba8ae7088565ce40c
Benjamin Spriggs <ben@sprico.com>
2019-02-10 19:01:19 -0800

    Keep title

--- src/views.ts
+++ src/views.ts
@@ -3,7 +3,9 @@ import express from 'express'
 var router: express.Router = express.Router()
 
 router.get("/", (_, res: express.Response) => {
-    res.render('home')
+    res.render('home', {
+        title: 'Home'
+    })
 })
 
 export default router
