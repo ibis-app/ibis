2b93ff33b7a016037189628d9eafd323f2fbd968
Benjamin Spriggs <ben@sprico.com>
2019-02-12 21:52:39 -0800

    Serve scripts and other public resources with caching

--- src/assets.ts
+++ src/assets.ts
@@ -4,7 +4,17 @@ import path from 'path'
 
 let router: express.Router = express.Router()
 
+router.use('/', express.static(path.join(config.paths.applicationRoot, 'dist', 'public'), {
+    index: false,
+    maxAge: '100000'
+}))
+
 // if this 404s, make sure to build fomantic-ui
-router.use('/semantic', express.static(path.join(config.paths.applicationRoot, 'semantic', 'dist')))
+router.use('/semantic', express.static(path.join(config.paths.applicationRoot, 'semantic', 'dist'), {
+    index: false,
+    setHeaders: (res) => {
+        res.setHeader('Date', new Date().toString())
+    }
+}))
 
 export default router
