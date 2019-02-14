1c594d342c824edf48ac693b9bef403085a535b3
Benjamin Spriggs <ben@sprico.com>
2019-02-13 23:04:53 -0800

    Move ui initialization code to app.ts

--- src/public/scripts/app.ts
+++ src/public/scripts/app.ts
@@ -1,2 +1,18 @@
 // import './reload'
 import './semantic.api'
+
+declare var $: JQueryStatic
+
+$('#modality-menu-toggle').click(() => {
+    let menu: any = $('#modality-menu') as any;
+    if (menu) {
+        menu.sidebar('toggle')
+    }
+});
+
+($('.ui.button.htm-link') as any).api({
+    encodeParameters: false,
+    onSuccess: (data: any) => {
+        console.dir(data)
+    }
+})
--- src/public/scripts/semantic.api.js
+++ src/public/scripts/semantic.api.js
@@ -5,10 +5,3 @@ $.api.settings.verbose = true
 $.api.settings.api = {
     'get htm': apiHostname + '/{filepath}/info'
 }
-
-$('.ui.button').api({
-    encodeParameters: false,
-    onSuccess: (response) => {
-        console.dir(response)
-    }
-})
