cd86bf33ca0d085bbcf34b836939d86f16f65f54
Benjamin Spriggs <ben@sprico.com>
2019-02-24 20:18:43 -0800

    Add caching headers, remove browserify from main

--- src/assets.ts
+++ src/assets.ts
@@ -1,25 +1,8 @@
 import express from 'express'
 import config from './api/config'
 import path from 'path'
-import browserify from 'browserify'
 
 let router: express.Router = express.Router()
-
-let b = browserify()
-b.add('./dist/public/scripts/app.js')
-
-router.get('/scripts/main.js', (_, res, next) => {
-    b.bundle((err, src) => {
-        if (err) {
-            console.error('failed to send bundle')
-            next(err)
-            return
-        }
-
-        res.type('script')
-        res.send(src.toString())
-    })
-})
 
 router.get('/favicon.ico', (_, res) => {
     res.sendFile(config.relative.ibisRoot('system', 'rsrcs', '32IBIS3.ico'))
@@ -27,15 +10,17 @@ router.get('/favicon.ico', (_, res) => {
 
 router.use('/', express.static(path.join(config.paths.applicationRoot, 'dist', 'public'), {
     index: false,
-    maxAge: '100000'
+    immutable: true,
+    maxAge: 10000,
+    lastModified: true
 }))
 
 // if this 404s, make sure to build fomantic-ui
 router.use('/semantic', express.static(path.join(config.paths.applicationRoot, 'semantic', 'dist'), {
     index: false,
-    setHeaders: (res) => {
-        res.setHeader('Date', new Date().toString())
-    }
+    immutable: true,
+    maxAge: 10000,
+    lastModified: true
 }))
 
 export default router
