7b97f51d24dcb86feeeee1186ff9f21235ce0d8b
bspriggs <ben@sprico.com>
2019-07-04 18:50:35 -0700

    Add more app

--- packages/ibis-web/src/index.ts
+++ packages/ibis-web/src/index.ts
@@ -1,2 +1,11 @@
 // TODO: use vue, vue-router, pouchdb to wire together a working app
+import Vue from "/web_modules/vue"
+
+var app = new Vue({
+    el: "#app",
+    data: {
+        magic: "Words!"
+    }
+});
+
 console.log("hello, world!")
