e0c83924c7cacff2e0c7795e2e86f6541aba7422
bspriggs <ben@sprico.com>
2019-07-04 19:49:32 -0700

    Close the process after restart

--- packages/ibis-web/gulpfile.js
+++ packages/ibis-web/gulpfile.js
@@ -6,14 +6,20 @@ const { project } = require("./../../gulpfile")
 
 const { build, buildFast, clean, compress, bundle, watch } = project(package)
 
-function watchStaticFiles() {
-    nodemon(package.nodemon)
+function watchStaticFiles(cb) {
+    var monitor = nodemon(package.nodemon)
         .on('start', () => {
             console.log('started watching')
         })
+        .on('error', () => process.exit(1))
+        .on('exit', () => {
+            cb();
+            process.exit(0)
+        })
 
-    process.on('SIGABRT', () => nodemon.emit('exit'))
-    process.on('SIGTERM', () => nodemon.emit('exit'))
+    process.on('SIGABRT', () => monitor.emit('exit'))
+    process.on('SIGTERM', () => monitor.emit('exit'))
+    process.on('uncaughtException', () => monitor.emit('exit'))
 }
 
 exports.build = build;
