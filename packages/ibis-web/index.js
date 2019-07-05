1c98bd7e319c4a3d393c886c233ebaed1340db84
bspriggs <ben@sprico.com>
2019-07-04 22:36:44 -0700

    Use express.static instead of http-server

--- packages/ibis-web/index.js
+++ packages/ibis-web/index.js
@@ -1,12 +1,15 @@
 //@ts-check
-const { createServer } = require("http-server");
+const express = require("express");
+
+var app = express();
 
 try {
-    createServer({
-        root: "dist",
-        showDotfiles: false,
-        gzip: true
-    }).listen(8080, 'localhost', () => {
+    app
+    .use(express.static("dist", {
+        dotfiles: "ignore",
+        extensions: ["html", "js"]
+    }))
+    .listen(8080, 'localhost', () => {
         console.log('listening on http://localhost:8080')
     })
 } catch (e) {
