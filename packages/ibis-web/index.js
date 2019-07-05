e35c898470233cfed2c2179e4228f81c9519aaff
bspriggs <ben@sprico.com>
2019-07-04 19:49:54 -0700

    Catch errors in index.js

--- packages/ibis-web/index.js
+++ packages/ibis-web/index.js
@@ -1,10 +1,15 @@
 //@ts-check
 const { createServer } = require("http-server");
 
-createServer({
-    root: "dist",
-    showDotfiles: false,
-    gzip: true
-}).listen(8080, 'localhost', () => {
-    console.log('listening on http://localhost:8080')
-});
+try {
+    createServer({
+        root: "dist",
+        showDotfiles: false,
+        gzip: true
+    }).listen(8080, 'localhost', () => {
+        console.log('listening on http://localhost:8080')
+    })
+} catch (e) {
+    console.error(e)
+    process.exit(1)
+}
