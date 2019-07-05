a400f79e8197327f7729b038093972b3a7dfdfe3
bspriggs <ben@sprico.com>
2019-07-04 19:34:30 -0700

    Add watch task

--- packages/ibis-web/gulpfile.js
+++ packages/ibis-web/gulpfile.js
@@ -1,12 +1,24 @@
 // @ts-check
 const package = require("./package.json")
+const { parallel } = require("gulp")
+const nodemon = require("nodemon")
 const { project } = require("./../../gulpfile")
 
 const { build, buildFast, clean, compress, bundle, watch } = project(package)
+
+function watchStaticFiles() {
+    nodemon(package.nodemon)
+        .on('start', () => {
+            console.log('started watching')
+        })
+
+    process.on('SIGABRT', () => nodemon.emit('exit'))
+    process.on('SIGTERM', () => nodemon.emit('exit'))
+}
 
 exports.build = build;
 exports.bundle = bundle;
 exports.buildFast = buildFast;
 exports.clean = clean;
 exports.compress = compress;
-exports.watch = watch;
+exports.watch = parallel(watch, watchStaticFiles)
